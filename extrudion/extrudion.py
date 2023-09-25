fd925971bc080abdbc11f53957442184443149da
Paride Azzari <paride.azzari@hest.ethz.ch>
2023-09-25 10:31:03 +0200

    remove sklearn bug fix

--- extrudion/extrudion.py
+++ extrudion/extrudion.py
@@ -9,7 +9,7 @@ def analyzeDirectory(folder_path: str = '.', cut_off = True, sample_thickness = 
     Give a folder path as a relative or absolute path, the script will analyze all the .TAR files found in the directory and return a DataFrame containing the results.
     Leaving returns the Current Working Directory.
     '''
-    from src.files import Folder
+    from .files import Folder
     
     results = pd.DataFrame()
     
@@ -32,8 +32,8 @@ def analyzeFile(filename:str, folder: str, cut_off: bool = True, sample_thicknes
     '''
     Give a filename and a folder as a relative or absolute path, the script will analyze the .TAR files found and return a DataFrame containing the results.
     '''
-    from src.files import File
-    from src.stress import Stress
+    from .files import File
+    from .stress import Stress
     file = File(filename, folder, sample_thickness)
     
     analysis = Stress(file, cut_off)
