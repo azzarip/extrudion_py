6da39bcc34a678e33dfc757c7bd6f64388e6d39a
Paride Azzari <paride.azzari@hest.ethz.ch>
2024-02-07 17:17:39 +0100

    changed structure of extrudion

--- extrudion/extrudion.py
+++ extrudion/extrudion.py
@@ -2,49 +2,31 @@
 #
 # Licensed under the MIT License. See LICENSE
 
-import pandas as pd
-
-def analyzeDirectory(folder_path: str = '.', cut_off = True, sample_area = 100) -> pd.DataFrame:
+def analyzeDirectory(folder_path: str = '.', options = []):
     '''
     Give a folder path as a relative or absolute path, the script will analyze all the .TAR files found in the directory and return a DataFrame containing the results.
     Leaving returns the Current Working Directory.
     '''
-    from .files import Folder
-    import os
+    from .files import TERFolder
+    return TERFolder(folder_path).analyze(options)
     
-    results = pd.DataFrame()
+
+
+
     
-    try:
-        files = Folder.getList(folder_path)
-       
-        if not os.path.exists('plots'):
-            os.makedirs('plots')
-        
-        for file in files:
-            dataFile = getFile(file, folder_path, sample_area)
-            result = analyzeFile(dataFile, cut_off)
-            results = pd.concat([results, result])
-            
-        results = results.rename_axis(index='File')
-        results.to_csv(folder_path+'/analysis.csv')
-        return results
-    
-    except FileNotFoundError as e:
-        print("File not found error:", e)
-    
-def analyzeFile(file, cut_off: bool = True):
+def analyzeFile(file, options = []):
     '''
     Give a filename and a folder as a relative or absolute path, the script will analyze the .TAR files found and return a DataFrame containing the results.
     '''    
-    from .stress import Stress
-        
-    analysis = Stress(file, cut_off)
+    # from .results import Result
+    from .files import TERFile
+
+    return TERFile(file)    
+
+
+    result = Result(File(file), options)
     analysis.plot()
     
     results = analysis.results
     results.index = [file.filename.replace('.TRA', '')]
     return results
-
-def getFile(filename: str, folder: str, sample_area = 100):
-    from .files import File
-    return File(filename, folder, sample_area)    
--- extrudion/files.py
+++ extrudion/files.py
@@ -1,13 +1,32 @@
-class Folder:
+class TERFolder:
     class FolderNotFound(Exception):
         pass
     
-    @staticmethod
-    def getList(folder_path: str) -> str:
+    def __init__(self, folder_path: str):
+        self.folder_path = folder_path
+        
+        self.file_list =self.getTERFilesInFolder()
+        
+        import pandas as pd
+        self.results = pd.DataFrame()
+        
+        import os
+        if not os.path.exists(folder_path + '/plots'):
+            os.makedirs(folder_path + '/plots') 
+    
+    def analyze(self, options = []):
+        import pandas as pd
+        
+        for file in self.file_list:
+            result = TERFile(self.folder_path + '/' + file).analyze(options)
+            self.results = pd.concat([self.results, result])
+    
+    
+    def getTERFilesInFolder(self):
         import os
             
         try:
-            dir_list = os.listdir(folder_path)
+            dir_list = os.listdir(self.folder_path)
             
             try:
                 file_list = [filename for filename in dir_list if filename.endswith('.TRA')]
@@ -23,39 +42,43 @@ class Folder:
                 raise
             
         except FileNotFoundError:
-            raise Folder.FolderNotFound('Folder not found') 
+            raise TERFolder.FolderNotFound('Folder not found') 
         
-
-class File:
-    import pandas as pd
     
-    def __init__(self, filename: str, folder: str, sample_area = 100):
-        self.filename = filename
-        self.folder = folder
-        self.sample_area = sample_area
-        self.data = self.calculate()
+
+class TERFile:
+    def __init__(self, filepath: str):
+        import pandas as pd
+        self.data = pd.read_table(filepath, header = [3], encoding = 'ANSI', sep = ',')
+    
+    def analyze(self, options = []):
+        pass
+    
+        
+# class Old:
+ 
                 
-    def openFile(self) -> pd.DataFrame:
-        import os 
-        file = os.path.join(self.folder, self.filename)
+#     def openFile(self) -> pd.DataFrame:
+#         import os 
+#         file = os.path.join(self.folder, self.filename)
         
-        import pandas as pd
-        return pd.read_table(file, header = [3], encoding = 'ANSI', sep = ',')
+#         import pandas as pd
+#         return pd.read_table(file, header = [3], encoding = 'ANSI', sep = ',')
         
-    def calculate(self) -> pd.DataFrame:
-        import pandas as pd
-        data = self.openFile()
+#     def calculate(self) -> pd.DataFrame:
+#         import pandas as pd
+#         data = self.openFile()
         
-        strain = self.getStrain(data)
-        stress = self.getStress(data)
+#         strain = self.getStrain(data)
+#         stress = self.getStress(data)
         
-        return pd.DataFrame({'strain': strain, 'stress': stress})
+#         return pd.DataFrame({'strain': strain, 'stress': stress})
         
-    def getStress(self, data) -> pd.Series:
-        # Divide force by surface in mm to get Pascals, divide by 1000 to get KPascals
-        return data['N'] / (self.sample_area) * 1_000_000 / 1000
+#     def getStress(self, data) -> pd.Series:
+#         # Divide force by surface in mm to get Pascals, divide by 1000 to get KPascals
+#         return data['N'] / (self.sample_area) * 1_000_000 / 1000
 
     
-    def getStrain(self, data) -> pd.Series:
-        import numpy as np
-        return np.log(data['mm.1'] / data['mm.1'][0])
+#     def getStrain(self, data) -> pd.Series:
+#         import numpy as np
+#         return np.log(data['mm.1'] / data['mm.1'][0])
