4275fb941e057267cd12a757be974e51008e0266
Paride Azzari <paride.azzari@hest.ethz.ch>
2024-02-14 14:41:21 +0100

    can plot files in folder

--- extrudion/extrudion.py
+++ extrudion/extrudion.py
@@ -7,8 +7,8 @@ def analyzeDirectory(folder_path: str = '.', options = {}):
     Give a folder path as a relative or absolute path, the script will analyze all the .TAR files found in the directory and return a DataFrame containing the results.
     Leaving returns the Current Working Directory.
     '''
-    from .files import TERFolder
-    return TERFolder(folder_path).analyze(options)
+    from .files import TRAFolder
+    return TRAFolder(folder_path).analyze(options)
     
 
 
@@ -19,6 +19,6 @@ def analyzeFile(file, options = {}):
     Give a filename and a folder as a relative or absolute path, the script will analyze the .TAR files found and return a DataFrame containing the results.
     '''    
     # from .results import Result
-    from .files import TERFile
+    from .files import TRAFile
 
-    return TERFile(file).analyze(options)
+    return TRAFile(file).analyze(options)
--- extrudion/files.py
+++ extrudion/files.py
@@ -1,11 +1,11 @@
-class TERFolder:
+class TRAFolder:
     class FolderNotFound(Exception):
         pass
     
     def __init__(self, folder_path: str):
         self.folder_path = folder_path
         
-        self.file_list =self.getTERFiles()
+        self.file_list =self.getTRAFiles()
         
         import pandas as pd
         self.results = pd.DataFrame()
@@ -18,11 +18,12 @@ class TERFolder:
         import pandas as pd
         
         for file in self.file_list:
-            result = TERFile(self.folder_path + '/' + file).analyze(options)
+            result = TRAFile(file, self.folder_path).analyze(options)
+            result['File'] = file.replace('.TRA', '')
             self.results = pd.concat([self.results, result])
-        return self.results
+        return self.results.set_index('File')
     
-    def getTERFiles(self):
+    def getTRAFiles(self):
         import os
             
         try:
@@ -42,23 +43,36 @@ class TERFolder:
                 raise
             
         except FileNotFoundError:
-            raise TERFolder.FolderNotFound('Folder not found') 
+            raise TRAFolder.FolderNotFound('Folder not found') 
         
     
 
-class TERFile:
+class TRAFile:
     
-    def __init__(self, filepath: str):
+    def __init__(self, file: str, folder_path: str):
+        import os
         import pandas as pd
-        df = pd.read_table(filepath, header = [3], encoding = 'ANSI', sep = ',')
+        self.filename = file.replace('.TRA', '')
+        
+        if folder_path:
+            self.filepath = os.path.join(folder_path, file)
+        else:
+            self.filepath = file
+            
+        df = pd.read_table(self.filepath, header = [3], encoding = 'ANSI', sep = ',')
         df['mm'] = df['mm'].apply(replace_negative_values)
         self.data = df
          
     def analyze(self, options):
         from .analyzers import StressStrain      
-        ss = StressStrain(self.data, sample_area=options['sample_area'], initial_length=options['initial_length'])
         from .fit import Fit
-        return Fit(ss).results
+        from .plot import Plot
+        
+        data = StressStrain(self.data, sample_area=options['sample_area'], initial_length=options['initial_length'])
+        fit = Fit(data)
+        if "plot" not in options or options["plot"] is not False:
+            Plot(fit, data, self.filename)
+        return fit.results
     
 def replace_negative_values(x):
     if x >= 0:
